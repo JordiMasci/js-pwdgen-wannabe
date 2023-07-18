5be6d49fb1c803701cdea27d9e13741b942f1c93
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-07-18 16:10:31 +0200

    feat: nome cognome colore

---
+++ JAVASCRIPT/FILE.js
@@ -0,0 +1,9 @@
+let userName = prompt('Inserisci il tuo nome')
+
+let userUsername = prompt('Inserisci il tuo cognome')
+
+let userColor = prompt('Inserisci il tuo colore preferito')
+
+alert ('userName ' + 'userUsername ' + 'userColor')
+
+console.log(userName + userUsername + userColor) 
